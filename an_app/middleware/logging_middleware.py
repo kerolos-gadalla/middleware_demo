9e7aea2508ee0ae44b7360e62fea38f3865ebf5c
Kerolos Gadalla <kerolos.sss@gmail.com>
2022-11-30 20:33:04 +0200

    print for logging

--- an_app/middleware/logging_middleware.py
+++ an_app/middleware/logging_middleware.py
@@ -1,15 +1,20 @@
 import logging
 
 logger = logging.getLogger(__name__)
+class Log:
+    def debug(self, *a, **kw):
+        return print(*a, **kw) 
+
+logger = Log()
 
 
 def loggingMiddlewareFactory(get_response):
     
     def loggingMiddleWare(request):
-        logger.debug("Just Got an Incoming request with Path {request.path}")
+        logger.debug(f"Just Got an Incoming request with Path {request.path} and id {id(request)}")
         response  = get_response(request)
-        logger.debug("Finished the request with response {response.data}")
-        logger.debug("Finished the request with response code {response.status_code}")
+        logger.debug(f"Finished the request with response {response}")
+        logger.debug(f"Finished the request with response code {response.status_code}")
         return response
         
     return loggingMiddleWare
